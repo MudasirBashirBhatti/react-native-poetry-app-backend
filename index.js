252d2cb179a0bd893b9434b1821f19e1a6592ea0
Mudasir <mudasir.bhatti358@gmail.com>
2024-09-19 09:24:01 +0500

    localhost origin removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json())
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    origin: [process.env.CLIENT_URL],
     methods: ['PUT', 'POST', 'DELETE', 'GET']
 }))
 
